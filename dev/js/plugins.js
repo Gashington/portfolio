68e46aa35d489c166f8b7e8e4bcc93c2d2b06536
Gashington <gashington@gmail.com>
2014-12-17 17:51:45 +0200

    add login.html and #login-form validation Fix contact.html adaptive

--- dev/js/plugins.js
+++ dev/js/plugins.js
@@ -1,8 +1,39 @@
-$(function(){
+$(function () {
+    // stylization input[type="file"]
+    $('input[type="file"]').styler()
+
+    // initialization tooltipster
+    $('#add-project-form input').tooltipster({
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: 21,
+        autoClose: true
+    });
+    $('#login-form input').tooltipster({
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'left',
+        offsetX: 21,
+        autoClose: true
+    });
+    // add validation method for url
+    $.validator.addMethod("complete_url", function (val, elem) {
+        if (val.length == 0) {
+            return true;
+        }
+
+        if (!/^(https?|ftp):\/\//i.test(val)) {
+            val = 'http://' + val; // set both the value
+            $(elem).val(val); // also update the form element
+        }
+        return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(val);
+    });
+
     // initialization fancybox with validation
     $(".fancybox").fancybox({
         padding: 0,
-        afterLoad: function(){
+        afterLoad: function () {
             // initialization validation
             $('#validate-form1').validate({
                 errorPlacement: function (error, element) {
@@ -11,7 +42,7 @@ $(function(){
                 },
                 success: function (label, element) {
                     $(element).tooltipster('hide');
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $('input[type="file"]').trigger('refresh');
                     }, 1);
                 },
@@ -38,65 +69,71 @@ $(function(){
                 submitHandler: function (form) {
                     return false;
                 },
-                invalidHandler: function() {
+                invalidHandler: function () {
 
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $('input[type="file"]').trigger('refresh');
                     }, 1)
 
                 }
             });
         },
-        beforeClose: function(){
+        beforeClose: function () {
             // hide tooltips
             $('#add-project-form input').tooltipster('hide');
         },
-        helpers:  {
-            overlay : {
-                locked : false
+        helpers: {
+            overlay: {
+                locked: false
             }
         }
+
+    //    validation #login-form
+
     });
 
-    // stylization input[type="file"]
-    $('input[type="file"]').styler()
 
-    // initialization tooltipster
-    $('#add-project-form input').tooltipster({
-        trigger: 'custom',
-        onlyOne: false,
-        position: 'left',
-        offsetX: 21,
-        autoClose: true
-    });
 
-    // add validation method for url
-    $.validator.addMethod("complete_url", function(val, elem) {
-        if (val.length == 0) { return true; }
-
-        if(!/^(https?|ftp):\/\//i.test(val)) {
-            val = 'http://'+val; // set both the value
-            $(elem).val(val); // also update the form element
+    $('#login-form form').validate({
+        errorPlacement: function (error, element) {
+            $(element).tooltipster('update', $(error).text());
+            $(element).tooltipster('show');
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+        },
+        rules: {
+            login: {
+                required: true
+            },
+            password: {
+                required: true
+            }
+        },
+        messages: {
+            login: "введите логин",
+            password: "введите пароль"
+        },
+        submitHandler: function (form) {
+            return false;
         }
-        return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&amp;'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(val);
     });
-
-   // js media queries
+    // js media queries
     enquire.register("screen and (max-width: 1050px)", {
-        match : function() {
+        match: function () {
             $('.menu-button').prepend($('.main-nav'));
             $('.for-contacts').prepend($('.w-contact'));
         },
-        unmatch : function() {
+        unmatch: function () {
             $('.sidebar-item-1').prepend($('.main-nav'));
             $('.sidebar-item-2').prepend($('.w-contact'));
         }
     });
     enquire.register("screen and (max-width: 721px)", {
-        match : function() {
+        match: function () {
             $('.for-social').prepend($('.social-list'));
         },
-        unmatch : function() {
+        unmatch: function () {
             $('.header-item-2').append($('.social-list'));
         }
     });
